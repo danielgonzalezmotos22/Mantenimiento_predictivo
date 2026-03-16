c4e05d0f231bcc5af04a3524444d16b8a2bd6ac3
danielgonzalezmotos22 <danielgonzalezmotos22@gmail.com>
2026-03-16 11:39:53 +0100

    commit de control

---
+++ src/api.py
@@ -0,0 +1,2 @@
+from fastapi import FastAPI
+from datetime import datetime
--- src/clases.py
+++ src/clases.py
@@ -1,5 +1,7 @@
 class SensorData:
-    def __init__(self, temperature, pressure, vibration):
-        self.temperature = temperature
-        self.pressure = pressure
+    def __init__(self, reactor_temp, reactor_pressure, vibration, ambient_temp_effect, feed_flow_rate):
+        self.reactor_temp = reactor_temp
+        self.reactor_pressure = reactor_pressure
         self.vibratin = vibration
+        self.ambient_temp_effect = ambient_temp_effect
+        self.feed_flow_rate = feed_flow_rate 
